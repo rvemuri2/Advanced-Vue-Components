bece5e271480e37c1b8144e88e1f72dfc1b4f2ca
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-04 22:17:22 -0800

    passing a prop in vue

--- components/src/App.vue
+++ components/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <p>Hey!</p>
   <Greeting> </Greeting>
+  <User :age="age"></User>
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue";
+import User from "@/components/User.vue";
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 20,
+    };
   },
 };
 </script>
