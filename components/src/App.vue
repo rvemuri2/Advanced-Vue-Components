6e9643f62b2250390ac42118e0339fa0b3c0b8a2
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-04 21:54:03 -0800

    making a vue app, with a component

---
+++ components/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Greeting></Greeting>
+</template>
+
+<script>
+import Greeting from "@/components/Greeting.vue";
+export default {
+  name: "App",
+  components: {
+    Greeting,
+  },
+};
+</script>
