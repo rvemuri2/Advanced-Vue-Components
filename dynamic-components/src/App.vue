9fac0b1560ce03235bef736a82765eccc156bab3
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-06 00:59:53 -0800

    dynamic components

---
+++ dynamic-components/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <KeepAlive>
+    <component :is="componentName"></component>
+  </KeepAlive>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    };
+  },
+};
+</script>
---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("It was unmounted");
+  },
+  activated() {
+    console.log("Home comp is activated");
+  },
+  deactivated() {
+    console.log("Home comp is de-activated");
+  },
+};
+</script>
